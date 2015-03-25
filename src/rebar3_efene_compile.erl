0583a428b3e096c090662fc92150ea1703ccc9c6
Mariano Guerra <mariano@marianoguerra.org>
2015-03-24 22:41:36 -0300

    add options to efene compile task

--- src/rebar3_efene_compile.erl
+++ src/rebar3_efene_compile.erl
@@ -20,7 +20,9 @@ init(State) ->
             {bare, false},
             {deps, ?DEPS},              % The list of dependencies
             {example, "rebar efene compile"}, % How to use the plugin
-            {opts, []},                  % list of options understood by the plugin
+            % list of options understood by the plugin
+            {opts, [{format, undefined, "format", string, help(format)},
+                    {file, undefined, "file", string, help(file)}]},
             {short_desc, "efene rebar3 plugin"},
             {desc, ""}
     ]),
@@ -29,7 +31,8 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    lists:foreach(fun compile_sources/1, rebar_state:project_apps(State)),
+    lists:foreach(fun (App) -> compile_sources(App, State) end,
+                  rebar_state:project_apps(State)),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
@@ -40,22 +43,52 @@ format_error(Reason) ->
 %% Private API
 %% ===================================================================
 
-compile(Path, DestPath) ->
-    io:format("Compiling ~s~n", [Path]),
+compile("rawlex", Path, _DestPath) ->
+    io:format("~P~n", [efene:to_raw_lex(Path), 1000]);
+compile("lex", Path, _DestPath) ->
+    io:format("~P~n", [efene:to_lex(Path), 1000]);
+compile("ast", Path, _DestPath) ->
+    io:format("~P~n", [efene:to_ast(Path), 1000]);
+compile("mod", Path, _DestPath) ->
+    io:format("~P~n", [efene:to_mod(Path), 1000]);
+compile("erl", Path, _DestPath) ->
+    io:format("~s~n", [efene:to_erl(Path)]);
+compile("erlast", Path, _DestPath) ->
+    Data = case efene:to_erl_ast(Path) of
+               {ok, {Ast, _State}} -> Ast;
+               Other -> Other
+           end,
+    io:format("~P~n", [Data, 1000]);
+compile("beam", Path, DestPath) ->
     case efene:compile(Path, DestPath) of
         {error, _}=Error ->
             Reason = fn_error:normalize(Error),
             io:format("error:~s~n", [Reason]);
         Other -> Other
-    end.
+    end;
+compile(Format, _Path, _DestPath) ->
+    io:format("Invalid format: ~s~n", [Format]).
 
-compile_sources(App) ->
+compile_sources(App, State) ->
     Path = filename:join(rebar_app_info:dir(App), "src"),
     DestPath = filename:join(rebar_app_info:dir(App), "ebin"),
     ok = filelib:ensure_dir(filename:join(DestPath, "a")),
     Mods = find_source_files(Path),
-    lists:foreach(fun (ModPath) -> compile(ModPath, DestPath) end, Mods),
+    {RawOpts, _} = rebar_state:command_parsed_args(State) ,
+    Format = proplists:get_value(format, RawOpts, "beam"),
+    case proplists:lookup(file, RawOpts) of
+        none ->
+            lists:foreach(fun (ModPath) ->
+                              io:format("Compiling ~s~n", [ModPath]),
+                              compile(Format, ModPath, DestPath)
+                          end, Mods);
+        {file, ModPath} ->
+            compile(Format, ModPath, DestPath)
+    end,
     ok.
 
 find_source_files(Path) ->
     [filename:join(Path, Mod) || Mod <- filelib:wildcard("*.fn", Path)].
+
+help(format) -> "format to compile code to, one of rawlex, lex, ast, mod, erlast, erl, beam";
+help(file) -> "file to compile, if omited all files in the project are compiled".
